8b8daefcf45838c077d5431229085f0a5a16389c
haingoctoday <33615962+haingoctoday@users.noreply.github.com>
2018-01-19 10:17:09 +0700

    1

--- README.md
+++
@@ -1,2 +0,0 @@
-sửa ođỏi tsi
-ngoc update
--- webroot/js/myjs.js
+++
@@ -1,2 +0,0 @@
-//$('.parallax-window1').parallax({imageSrc: 'http://localhost:8765/img/1.jpg'});
-//$('.parallax-window').parallax({imageSrc: 'http://localhost:8765/img/1.jpg'});
